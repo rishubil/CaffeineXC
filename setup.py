b9c57d7155b0728a1184df3aa1f77e10aa7be26e
Nesswit <rishubil@gmail.com>
2019-06-20 23:24:39 +0900

    Init project

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from cx_Freeze import setup, Executable
+
+buildOptions = dict(packages=["numpy.core", "numpy.lib"])
+
+executables = [Executable("xbox-keep-on.py")]
+
+setup(
+    name="xbox-keep-on",
+    version="0.2",
+    description="xbox-keep-on",
+    options=dict(build_exe=buildOptions),
+    executables=executables,
+)
